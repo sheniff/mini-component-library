78c7c8ff25e8ef315c1e02393adbb9dbd34830f8
Sergio Almecija <sheniff@dropbox.com>
2022-07-24 23:55:25 -0700

    fix progressbar based on solution + first iteration select

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,49 +1,59 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <Wrapper aria-label='progress' role='progressbar' aria-valuenow={value} style={SIZES[size]}><Progress value={value}/></Wrapper>;
+  return (
+    <Wrapper
+      aria-label="progress"
+      role="progressbar"
+      aria-valuenow={value}
+      style={SIZES[size]}
+    >
+      <BarWrapper>
+        <Progress value={value} />
+      </BarWrapper>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+    </Wrapper>
+  );
 };
 
 const SIZES = {
   large: {
-    '--height': '24px',
-    '--radius': '8px',
-    '--padding': '4px',
+    "--height": "24px",
+    "--padding": "4px",
+    "--radius": "8px",
   },
   medium: {
-    '--height': '12px',
-    '--radius': '4px',
+    "--height": "12px",
+    "--radius": "4px",
   },
   small: {
-    '--height': '8px',
-    '--radius': '4px',
+    "--height": "8px",
+    "--radius": "4px",
   },
-}
+};
 
 const Wrapper = styled.div`
-  width: 370px;
-  height: var(--height);
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   background-color: ${COLORS.transparentGray15};
   border-radius: var(--radius);
-  overflow: hidden;
   padding: var(--padding);
 `;
 
-const Progress = styled.div`
-  background-color: ${COLORS.primary};
-  width: ${p => p.value}%;
-  height: 100%;
-  border-radius: 4px ${p => getEndRadius(p.value)}px ${p => getEndRadius(p.value)}px 4px;
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
 `;
 
-function getEndRadius(value) {
-  return value === 100 ? 4 : value > 99.5 ? 2 : 0;
-}
+const Progress = styled.div`
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  width: ${(p) => p.value}%;
+  border-radius: 4px 0 0 4px;
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,57 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <Text>{displayedValue}</Text>
+      <Icon id="chevron-down" size={24} strokeWidth={2} />
+      <SelectStyled id="selectthing" value={value} onChange={onChange}>
+        {children}
+      </SelectStyled>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  padding: 12px 12px 12px 16px;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  float: left;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:focus {
+    outline: 2px solid ${COLORS.primary};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Text = styled.div`
+  margin-right: 20px;
+  font: "Roboto" sans-serif;
+`;
+
+const SelectStyled = styled.select`
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1;
+`;
 
 export default Select;
